ca4db44b67a7cd0268c8f2222c8714f4b72d04d9
cah4a <cah4o3@gmail.com>
2024-02-25 10:40:53 +0200

    revert example to use only @rekube/base

--- example/k8s.tsx
+++ example/k8s.tsx
@@ -1,10 +1,32 @@
-import { Postgres } from "@rekube/components";
-import { NamespaceProvider } from "rekube";
+import { Deployment } from "@rekube/base/apps/v1";
+import {
+    Container,
+    ContainerPort,
+    PodTemplateSpec,
+    Service,
+    ServicePort
+} from "@rekube/base/core/v1";
 
-export default function MyCloud() {
+export default function App() {
+    const labels = { app: "nginx" };
+
     return (
-        <NamespaceProvider namespace="my-namespace">
-            <Postgres name="my-postgres" />
-        </NamespaceProvider>
+        <>
+            <Deployment
+                meta:name="nginx"
+                replicas={3}
+                selector={{ matchLabels: labels }}
+            >
+                <PodTemplateSpec meta:labels={labels}>
+                    <Container name="nginx" image="nginx:1.14.2">
+                        <ContainerPort containerPort={80} />
+                    </Container>
+                </PodTemplateSpec>
+            </Deployment>
+
+            <Service meta:name="nginx-svc" selector={{ labels }}>
+                <ServicePort port={80} targetPort={80} protocol="TCP" />
+            </Service>
+        </>
     );
 }
